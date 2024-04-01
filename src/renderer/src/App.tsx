85f2deea5058b2c23e4842201b9fe274abe2353d
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-04-01 15:04:58 +0330

    feat:create markdown

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,6 +1,8 @@
 import { ActionButtonsRow } from './components/ActionButtonsRow'
 import { Content, RootLayout, Sidebar } from './components/AppLayout'
 import { DraggableTopBar } from './components/DraggableTopBar'
+import { MarkdownEditor } from './components/MarkdownEditor'
+import { NotePreviewList } from './components/NotePreviewList'
 
 function App(): JSX.Element {
   return (
@@ -8,9 +10,12 @@ function App(): JSX.Element {
       <DraggableTopBar />
       <RootLayout className="bg-black bg-opacity-10">
         <Sidebar className="p-2   ">
-          <ActionButtonsRow className="flex justify-between" />
+          <ActionButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList />
         </Sidebar>
-        <Content className=" bg-black bg-zing-900/50 border-l border-l-white/20">content</Content>
+        <Content className=" bg-black bg-zing-900/50 border-l border-l-white/20">
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )

95c53a80776043d24802e8b17195dd07a4d78088
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-04-01 12:50:39 +0330

    feat:add showing notes componets

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -5,3 +5,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'UTC'
+})
+
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)

0c97a4800b010e2f428522c25c2462c68c515fd6
Mahdi Parivash <mahdiparivash@protonmail.com>
2024-04-01 12:02:24 +0330

    feat:add deagable component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,16 @@
+import { Content, RootLayout, Sidebar } from './components/AppLayout'
+import { DraggableTopBar } from './components/DraggableTopBar'
+
 function App(): JSX.Element {
-  return <div></div>
+  return (
+    <>
+      <DraggableTopBar />
+      <RootLayout className="bg-black bg-opacity-10">
+        <Sidebar className="p-2   ">SideBar</Sidebar>
+        <Content className=" bg-black bg-zing-900/50 border-l border-l-white/20">content</Content>
+      </RootLayout>
+    </>
+  )
 }
 
 export default App
---
+++ src/renderer/src/components/DraggableTopBar.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const DraggableTopBar: React.FC = () => {
+  return <header className="absolute inset-0 h-8 bg-transparent" />
+}
